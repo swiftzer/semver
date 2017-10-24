5e6c1e27ee53a6a8ed5ce316b031ace0405dfb96
Eric Li <eric@swiftzer.net>
2017-10-24 23:16:35 +0800

    Rename all test methods

--- src/test/java/net/swiftzer/semver/SemVerTest.kt
+++ src/test/java/net/swiftzer/semver/SemVerTest.kt
@@ -9,237 +9,237 @@ import kotlin.test.assertTrue
 
 class SemVerTest {
     @Test
-    fun init_valid() {
+    fun initValid() {
         SemVer(12, 23, 34, "alpha.12", "test.34")
     }
 
     @Test
-    fun init_invalid_major() {
+    fun initInvalidMajor() {
         assertFails { SemVer(-1, 23, 34, "alpha.12", "test.34") }
     }
 
     @Test
-    fun init_invalid_minor() {
+    fun initInvalidMinor() {
         assertFails { SemVer(12, -1, 34, "alpha.12", "test.34") }
     }
 
     @Test
-    fun init_invalid_patch() {
+    fun initInvalidPatch() {
         assertFails { SemVer(12, 23, -1, "alpha.12", "test.34") }
     }
 
     @Test
-    fun init_invalid_preRelease() {
+    fun initInvalidPreRelease() {
         assertFails { SemVer(12, 23, 34, "alpha.12#", "test.34") }
     }
 
     @Test
-    fun init_invalid_metadata() {
+    fun initInvalidMetadata() {
         assertFails { SemVer(12, 23, 34, "alpha.12", "test.34#") }
     }
 
     @Test
-    fun parse_numeric() {
+    fun parseNumeric() {
         val actual = SemVer.parse("1.0.45")
         val expected = SemVer(1, 0, 45)
         assertEquals(expected, actual)
     }
 
     @Test
-    fun parse_preRelease() {
+    fun parsePreRelease() {
         val actual = SemVer.parse("1.0.0-alpha.beta-a.12")
         val expected = SemVer(1, 0, 0, preRelease = "alpha.beta-a.12")
         assertEquals(expected, actual)
     }
 
     @Test
-    fun parse_metadata() {
+    fun parseMetadata() {
         val actual = SemVer.parse("1.0.0+exp.sha-part.5114f85")
         val expected = SemVer(1, 0, 0, buildMetadata = "exp.sha-part.5114f85")
         assertEquals(expected, actual)
     }
 
     @Test
-    fun parse_all() {
+    fun parseAll() {
         val actual = SemVer.parse("1.0.0-beta+exp.sha.5114f85")
         val expected = SemVer(1, 0, 0, preRelease = "beta", buildMetadata = "exp.sha.5114f85")
         assertEquals(expected, actual)
     }
 
     @Test
-    fun parse_invalid() {
+    fun parseInvalid() {
         assertFails { SemVer.parse("1.0.1.4-beta+exp.sha.5114f85") }
     }
 
     @Test
-    fun isInitialDevelopmentPhase_true() {
+    fun isInitialDevelopmentPhaseTrue() {
         assertTrue { SemVer(0, 23, 34, "alpha.123", "testing.123").isInitialDevelopmentPhase() }
     }
 
     @Test
-    fun isInitialDevelopmentPhase_false() {
+    fun isInitialDevelopmentPhaseFalse() {
         assertFalse { SemVer(1, 23, 34, "alpha.123", "testing.123").isInitialDevelopmentPhase() }
     }
 
     @Test
-    fun toString_numeric() {
+    fun toStringNumeric() {
         val semVer = SemVer(1, 0, 45)
         assertEquals("1.0.45", semVer.toString())
     }
 
     @Test
-    fun toString_preRelease() {
+    fun toStringPreRelease() {
         val semVer = SemVer(1, 0, 0, preRelease = "alpha.beta-a.12")
         assertEquals("1.0.0-alpha.beta-a.12", semVer.toString())
     }
 
     @Test
-    fun toString_metadata() {
+    fun toStringMetadata() {
         val semVer = SemVer(1, 0, 0, buildMetadata = "exp.sha-part.5114f85")
         assertEquals("1.0.0+exp.sha-part.5114f85", semVer.toString())
     }
 
     @Test
-    fun toString_all() {
+    fun toStringAll() {
         val semVer = SemVer(1, 0, 0, preRelease = "beta", buildMetadata = "exp.sha.5114f85")
         assertEquals("1.0.0-beta+exp.sha.5114f85", semVer.toString())
     }
 
     @Test
-    fun compareTo_numeric1() {
+    fun compareToNumeric1() {
         val semVer1 = SemVer(1, 0, 0)
         val semVer2 = SemVer(1, 0, 0)
         assertEquals(0, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_numeric2() {
+    fun compareToNumeric2() {
         val semVer1 = SemVer(1, 0, 0)
         val semVer2 = SemVer(2, 0, 0)
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_numeric3() {
+    fun compareToNumeric3() {
         val semVer1 = SemVer(2, 0, 0)
         val semVer2 = SemVer(2, 1, 0)
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_numeric4() {
+    fun compareToNumeric4() {
         val semVer1 = SemVer(2, 1, 4)
         val semVer2 = SemVer(2, 1, 0)
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_numeric5() {
+    fun compareToNumeric5() {
         val semVer1 = SemVer(2, 0, 0)
         val semVer2 = SemVer(1, 0, 0)
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_numeric6() {
+    fun compareToNumeric6() {
         val semVer1 = SemVer(1, 2, 0)
         val semVer2 = SemVer(1, 0, 0)
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_numeric7() {
+    fun compareToNumeric7() {
         val semVer1 = SemVer(1, 0, 0)
         val semVer2 = SemVer(1, 0, 2)
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease1() {
+    fun compareToPreRelease1() {
         val semVer1 = SemVer(1, 0, 0)
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha")
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease2() {
+    fun compareToPreRelease2() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha")
         val semVer2 = SemVer(1, 0, 0)
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease3() {
+    fun compareToPreRelease3() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha.1")
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease4() {
+    fun compareToPreRelease4() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha.1")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha")
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease5() {
+    fun compareToPreRelease5() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha.1")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha.beta")
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease6() {
+    fun compareToPreRelease6() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha.beta")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha.1")
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease7() {
+    fun compareToPreRelease7() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha.1")
         val semVer2 = SemVer(1, 0, 0, preRelease = "beta")
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease8() {
+    fun compareToPreRelease8() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "beta")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha.1")
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease9() {
+    fun compareToPreRelease9() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha.1")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha.2")
         assertEquals(-1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease10() {
+    fun compareToPreRelease10() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha.2")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha.1")
         assertEquals(1, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease11() {
+    fun compareToPreRelease11() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha.1")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha.1")
         assertEquals(0, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease12() {
+    fun compareToPreRelease12() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha")
         assertEquals(0, semVer1.compareTo(semVer2))
     }
 
     @Test
-    fun compareTo_preRelease_metadata() {
+    fun compareToPreReleaseMetadata() {
         val semVer1 = SemVer(1, 0, 0, preRelease = "alpha", buildMetadata = "xyz")
         val semVer2 = SemVer(1, 0, 0, preRelease = "alpha", buildMetadata = "abc")
         assertEquals(0, semVer1.compareTo(semVer2))
