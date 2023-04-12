c0fd9a63fb5ae2fcad7ecdbf13cf8abc13d2c424
rbbl-dev <dev@rbbl.cc>
2023-04-13 00:11:56 +0200

    added publishing to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     kotlin("multiplatform") version "1.8.20"
     id("org.jetbrains.dokka") version "1.8.10"
     jacoco
+    signing
+    `maven-publish`
 }
 
 group = "net.swiftzer.semver"
@@ -16,12 +18,15 @@ repositories {
 kotlin {
     jvm {
         jvmToolchain(8)
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
         withJava()
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
         }
     }
-    js(BOTH) {
+    js(IR) {
         browser {
             commonWebpackConfig {
                 cssSupport {
@@ -56,7 +61,67 @@ kotlin {
     }
 }
 
-tasks.withType<DokkaTask>().configureEach {
-    // custom output directory
-    outputDirectory.set(buildDir.resolve("dokka"))
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    dependsOn(tasks.dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaHtml)
+}
+
+val ossrhUsername: String? = System.getenv("OSSRH_USERNAME")
+val ossrhPassword: String? = System.getenv("OSSRH_PASSWORD")
+publishing {
+    publications {
+        repositories {
+            maven {
+                name="Central"
+                val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                credentials {
+                    username = ossrhUsername
+                    password = ossrhPassword
+                }
+            }
+        }
+        repositories {
+            maven {
+                name="Jfrog"
+                val releasesRepoUrl = uri("https://rbbl.jfrog.io/artifactory/default-maven-local")
+                val snapshotsRepoUrl = uri("https://rbbl.jfrog.io/artifactory/default-maven-local")
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                credentials {
+                    username = ossrhUsername
+                    password = ossrhPassword
+                }
+            }
+        }
+    }
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("SemVer")
+                description.set("Kotlin data class for Semantic Versioning 2.0.0")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/mit-license.php")
+                    }
+                }
+                url.set("https://github.com/swiftzer/semver")
+                scm {
+                    connection.set("scm:git:github.com/swiftzer/semver.git")
+                    developerConnection.set("scm:git:ssh://github.com/swiftzer/semver.git")
+                    url.set("https://github.com/swiftzer/semver/tree/master")
+                }
+                developers {
+                    developer {
+                        id.set("ericksli")
+                        name.set("Eric Li")
+                        email.set("eric@swiftzer.net")
+                    }
+                }
+            }
+        }
+    }
 }
