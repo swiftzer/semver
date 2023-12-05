d045a5c84faf1e1cae552f907e04d2a635a671b9
Goooler <wangzongler@gmail.com>
2023-12-06 00:04:51 +0800

    Always publish build scans on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,4 +11,18 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version "3.15.1"
+}
+
+val isCiBuild = providers.environmentVariable("CI").isPresent
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(isCiBuild)
+    }
+}
+
 rootProject.name = "semver"
